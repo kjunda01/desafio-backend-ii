c01fc08fe468079f980a1c21cff6cb1c1df79d6f
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-05-19 16:33:45 -0300

    Criação de cache para rotas de clientes

--- backend/controllers/clientesController.js
+++ backend/controllers/clientesController.js
@@ -1,13 +1,11 @@
+import { cacheService } from "../services/cacheService.js";
 import { clientesService } from "../services/clientesService.js";
-
-const create = async (req, res) => {
-  const { cliente } = req.body;
-  const resultado = await await clientesService.create(cliente);
-  return resultado ? res.status(200).json() : res.status(400).json({ message: "Falha ao salvar cliente" });
-};
+import chalk from "chalk";
 
 const readAll = async (req, res) => {
   const resultado = await await clientesService.readAll();
+  cacheService.flush();
+  console.log(chalk.yellowBright("[CACHE] Cache invalidado após GET"));
   return resultado ? res.status(200).json(resultado) : res.status(400).json({ message: "Falha ao encontrar clientes" });
 };
 
@@ -15,17 +13,31 @@ const readSingle = async (req, res) => {
   console.log(req.params);
   const { id } = req.params;
   const resultado = await await clientesService.readSingle(id);
+  cacheService.flush();
+  console.log(chalk.yellowBright("[CACHE] Cache invalidado após GET"));
   return resultado ? res.status(200).json() : res.status(400).json({ message: "Falha ao encontrar cliente específico" });
+};
+
+const create = async (req, res) => {
+  const { cliente } = req.body;
+  const resultado = await await clientesService.create(cliente);
+  cacheService.flush();
+  console.log(chalk.yellowBright("[CACHE] Cache invalidado após POST"));
+  return resultado ? res.status(200).json() : res.status(400).json({ message: "Falha ao salvar cliente" });
 };
 
 const update = async (req, res) => {
   const resultado = await await clientesService.update(req.body);
+  cacheService.flush();
+  console.log(chalk.yellowBright("[CACHE] Cache invalidado após PUT"));
   return resultado ? res.status(200).json() : res.status(400).json({ message: "Falha ao atualizar cliente" });
 };
 
 const remove = async (req, res) => {
   const { id } = req.params;
   const resultado = await await clientesService.remove(id);
+  cacheService.flush();
+  console.log(chalk.yellowBright("[CACHE] Cache invalidado após DELETE"));
   return resultado ? res.status(200).json() : res.status(400).json({ message: "Falha ao remover cliente" });
 };
 
--- backend/routes/clientesRoutes.js
+++ backend/routes/clientesRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { clientesController } from "../controllers/clientesController.js";
 import { clientesMiddleware } from "../middlewares/clientesMiddleware.js";
+import cacheMiddleware from "../middlewares/cacheMiddleware.js";
 
 const clientesRoutes = Router();
 
-clientesRoutes.post("/", clientesMiddleware.validaCliente, clientesController.create);
-clientesRoutes.get("/", clientesController.readAll);
-clientesRoutes.get("/:id", clientesController.readSingle);
-clientesRoutes.put("/:id", clientesController.update);
-clientesRoutes.delete("/:id", clientesController.remove);
+clientesRoutes.get("/", cacheMiddleware, clientesController.readAll);
+clientesRoutes.get("/:id", cacheMiddleware, clientesController.readSingle);
+clientesRoutes.post("/", cacheMiddleware, clientesMiddleware.validaCliente, clientesController.create);
+clientesRoutes.put("/:id", cacheMiddleware, clientesController.update);
+clientesRoutes.delete("/:id", cacheMiddleware, clientesController.remove);
 
 export default clientesRoutes;
